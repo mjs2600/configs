c4155ed2ab3fd338effc4edfdf081a84747a2411
Michael Simpson <michael@wearethorn.org>
2022-01-14 10:26:06 -0500

    changes from michaels-mbp.lan on Fri Jan 14 10:26:06 EST 2022

--- link_configs.py
+++ link_configs.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
 import os
+import sys
 from pathlib import Path
 
-import yaml
+from sh import brew, pip
 
+CONFIG_DIR: Path = Path(__file__).resolve().parent
 
 def link_required(source_file: Path, target_file: Path) -> bool:
     if target_file.exists():
@@ -14,24 +16,31 @@ def link_required(source_file: Path, target_file: Path) -> bool:
 
     return True
 
+def mac_setup():
+    print(f"Updating brew bundle")
+    print(brew.bundle(file=CONFIG_DIR / 'Brewfile'))
 
 def main():
-    with open("dirs.yaml") as f:
+    pip.install("pyyaml")
+    import yaml
+
+    with open(CONFIG_DIR / "dirs.yaml") as f:
         links = yaml.full_load(f)
-    config_dir = Path.cwd()
 
     for dir, files in links.items():
         destination_dir = Path(os.path.expanduser(dir))
         destination_dir.mkdir(parents=True, exist_ok=True)
         for file in files:
             target_file = destination_dir / os.path.basename(file)
-            source_file = config_dir / file
+            source_file = CONFIG_DIR / file
             if link_required(source_file, target_file):
                 target_file.symlink_to(source_file)
                 print(f"🖇️ Link Created:\t\t{target_file}")
             else:
                 print(f"🔗 Already Linked:\t {target_file}")
 
+    if sys.platform == "darwin":
+        mac_setup()
 
 if __name__ == "__main__":
     main()
