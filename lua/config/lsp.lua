c08c7732d275ea82b97b7e6cdaaa54b6c37ad15c
Michael Simpson <michael@higherorder.fun>
2024-11-19 20:37:46 -0500

    Format on Save

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -61,3 +61,15 @@ require('mason-lspconfig').setup({
 		"vale_ls",
 	},
 })
+
+-- Format the buffer before saving it
+vim.api.nvim_create_autocmd("BufWritePre", {
+	callback = function()
+		local mode = vim.api.nvim_get_mode().mode
+		local filetype = vim.bo.filetype
+		if vim.bo.modified == true and mode == 'n' and filetype ~= "oil" then
+			vim.cmd('lua vim.lsp.buf.format()')
+		else
+		end
+	end
+})
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -20,6 +20,106 @@ return {
 		},
 		-- See Commands section for default commands if you want to lazy load on them
 	},
+	{
+		"epwalsh/obsidian.nvim",
+		version = "*", -- recommended, use latest release instead of latest commit
+		lazy = true,
+		ft = "markdown",
+		-- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+		-- event = {
+		--   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+		--   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+		--   -- refer to `:h file-pattern` for more examples
+		--   "BufReadPre path/to/my-vault/*.md",
+		--   "BufNewFile path/to/my-vault/*.md",
+		-- },
+		dependencies = {
+			-- Required.
+			"nvim-lua/plenary.nvim",
+
+			-- see below for full list of optional dependencies 👇
+		},
+		opts = {
+			workspaces = {
+				{
+					name = "personal",
+					path = "~/notes/personal",
+				},
+				{
+					name = "work",
+					path = "~/notes/work",
+				},
+			},
+
+		},
+	},
+	{
+		"epwalsh/pomo.nvim",
+		version = "*", -- Recommended, use latest release instead of latest commit
+		lazy = true,
+		cmd = { "TimerStart", "TimerRepeat", "TimerSession" },
+		dependencies = {
+			-- Optional, but highly recommended if you want to use the "Default" timer
+			"rcarriga/nvim-notify",
+		},
+		opts = {
+			-- See below for full list of options 👇
+			-- How often the notifiers are updated.
+			update_interval = 1000,
+
+			-- Configure the default notifiers to use for each timer.
+			-- You can also configure different notifiers for timers given specific names, see
+			-- the 'timers' field below.
+			notifiers = {
+				-- The "Default" notifier uses 'vim.notify' and works best when you have 'nvim-notify' installed.
+				{
+					name = "Default",
+					opts = {
+						-- With 'nvim-notify', when 'sticky = true' you'll have a live timer pop-up
+						-- continuously displayed. If you only want a pop-up notification when the timer starts
+						-- and finishes, set this to false.
+						sticky = true,
+
+						-- Configure the display icons:
+						title_icon = "󱎫",
+						text_icon = "󰄉",
+						-- Replace the above with these if you don't have a patched font:
+						-- title_icon = "⏳",
+						-- text_icon = "⏱️",
+					},
+				},
+
+				-- The "System" notifier sends a system notification when the timer is finished.
+				-- Available on MacOS and Windows natively and on Linux via the `libnotify-bin` package.
+				{ name = "System" },
+
+				-- You can also define custom notifiers by providing an "init" function instead of a name.
+				-- See "Defining custom notifiers" below for an example 👇
+				-- { init = function(timer) ... end }
+			},
+
+			-- Override the notifiers for specific timer names.
+			timers = {
+				-- For example, use only the "System" notifier when you create a timer called "Break",
+				-- e.g. ':TimerStart 2m Break'.
+				Break = {
+					{ name = "System" },
+				},
+			},
+			-- You can optionally define custom timer sessions.
+			sessions = {
+				-- Example session configuration for a session called "pomodoro".
+				pomodoro = {
+					{ name = "Work",        duration = "25m" },
+					{ name = "Short Break", duration = "5m" },
+					{ name = "Work",        duration = "25m" },
+					{ name = "Short Break", duration = "5m" },
+					{ name = "Work",        duration = "25m" },
+					{ name = "Long Break",  duration = "15m" },
+				},
+			},
+		},
+	},
 	'hrsh7th/cmp-buffer',
 	'hrsh7th/cmp-cmdline',
 	'hrsh7th/cmp-nvim-lsp',
